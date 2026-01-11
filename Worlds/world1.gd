aa72bf30acc4582077fa198b07e74b1fb6fd368f
baihaqibb <128576782+dragontheminerz45@users.noreply.github.com>
2026-01-11 10:55:13 +0700

    Refactoring enemies

--- Worlds/world1.gd
+++ Worlds/world1.gd
@@ -6,10 +6,12 @@ extends Node2D
 func _on_boss_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and boss:
 		for gate in gates.get_children():
+			if gate is not StaticBody2D: return
 			gate.visible = true
 			gate.get_child(0).set_deferred("disabled", false)
 
 func _on_boss_defeated() -> void:
 	for gate in gates.get_children():
+		if gate is not StaticBody2D: return
 		gate.visible = false
 		gate.get_child(0).set_deferred("disabled", true)
